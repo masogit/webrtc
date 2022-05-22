f499186b7243bf57750865f1c178cfe1af548f45
Man, Shuo <shuo.man@ge.com>
2022-05-22 17:14:06 +0000

    client list同步

--- index.js
+++ index.js
@@ -6,21 +6,36 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let clients = []
+const addClient = id => {
+  if (clients.indexOf(id) < 0) {
+    clients.push(id)
+  }
+  return clients
+}
+const delClient = id => {
+  clients = clients.filter(_ => _ != id)
+  return clients
+}
+
 app.get("/channel", (req, res) => {
-    res.sendFile(__dirname + "/channel/index.html");
+  res.sendFile(__dirname + "/channel/index.html");
 });
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 app.use(express.static(path.join(__dirname, "channel")));
 
 io.on("connection", (socket) => {
   console.log("a user connected: socket.id", socket.id);
   socket.broadcast.emit("hi");
-  io.emit("socket.id", socket.id);
+  io.emit("socket.id", addClient(socket.id));
+
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    io.emit("socket.id", delClient(socket.id));
+    console.log("user disconnected", socket.id);
   });
+
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);

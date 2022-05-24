849e93eeb190571f16e904df47bd51e468d92c22
Man, Shuo <shuo.man@ge.com>
2022-05-24 16:45:38 +0000

    format

--- index.js
+++ index.js
@@ -8,76 +8,76 @@ const io = new Server(server);
 
 let clients = []
 const addClient = id => {
-  if (clients.indexOf(item => item.id == id) < 0) {
-    clients.push({ id, name: null })
-  }
-  return clients
+    if (clients.indexOf(item => item.id == id) < 0) {
+        clients.push({ id, name: null })
+    }
+    return clients
 }
 const delClient = id => {
-  clients = clients.filter(item => item.id != id)
-  return clients
+    clients = clients.filter(item => item.id != id)
+    return clients
 }
 const setName = ({ id, name }) => {
-  console.log('setName', { id, name }, clients)
-  const _ = clients.find(item => item.id == id)
-  if (_) {
-    _.name = name
-  }
+    console.log('setName', { id, name }, clients)
+    const _ = clients.find(item => item.id == id)
+    if (_) {
+        _.name = name
+    }
 }
 const setCallName = ({ id, _id }) => {
-  const self = clients.find(item => item.id == id)
-  const to = clients.find(item => item.id == _id)
-  if (self && to) {
-    self.calling = to.name
-    to.incoming = self.name
-  }
+    const self = clients.find(item => item.id == id)
+    const to = clients.find(item => item.id == _id)
+    if (self && to) {
+        self.calling = to.name
+        to.incoming = self.name
+    }
 }
 
 app.get("/channel", (req, res) => {
-  res.sendFile(__dirname + "/channel/index.html");
+    res.sendFile(__dirname + "/channel/index.html");
 });
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 app.use(express.static(path.join(__dirname, "channel")));
 
 io.on("connection", (socket) => {
-  console.log("a user connected: socket.id", socket.id);
-  socket.broadcast.emit("hi");
-  io.emit("socket.id", addClient(socket.id))
-  // io.emit("socket.id", addClient(socket.id));
+    console.log("a user connected: socket.id", socket.id);
+    socket.broadcast.emit("hi");
+    io.emit("socket.id", addClient(socket.id))
+    // io.emit("socket.id", addClient(socket.id));
 
-  socket.on("disconnect", () => {
-    io.emit("socket.id", delClient(socket.id));
-    console.log("user disconnected", socket.id);
-  });
-
-  socket.on("setName", (name) => {
-    setName({ id: socket.id, name })
-    io.emit("socket.id", clients);
-    // io.emit("chat message", msg);
-  });
-
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
-  });
-
-  socket.on("call-user", (data) => {
-    socket.to(data.to).emit("call-made", {
-      offer: data.offer,
-      socket: data.from,
+    socket.on("disconnect", () => {
+        io.emit("socket.id", delClient(socket.id));
+        console.log("user disconnected", socket.id);
     });
-  });
-  socket.on("make-answer", (data) => {
-    setCallName({ id: data.from, to: data.from })
-    socket.to(data.to).emit("answer-made", {
-      socket: data.from,
-      answer: data.answer,
+
+    socket.on("setName", (name) => {
+        setName({ id: socket.id, name })
+        io.emit("socket.id", clients);
+        // io.emit("chat message", msg);
     });
-  });
+
+    socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+    });
+
+    socket.on("call-user", (data) => {
+        socket.to(data.to).emit("call-made", {
+            offer: data.offer,
+            socket: data.from,
+        });
+    });
+    socket.on("make-answer", (data) => {
+        setCallName({ id: data.from, to: data.from })
+        socket.to(data.to).emit("answer-made", {
+            socket: data.from,
+            answer: data.answer,
+        });
+    });
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+    console.log("listening on *:3000");
 });
